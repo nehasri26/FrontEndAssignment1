b3935677b2b7dfe9d197c623194a4abb56079319
nehasri26 <95169443+nehasri26@users.noreply.github.com>
2022-05-15 23:46:25 +0530

    Added code

---
+++ PharmaWebsite.js
@@ -0,0 +1,9 @@
+function openForm() {
+    console.log("OpenForm");
+    document.getElementById("labForm").style.display = "flex";
+  }
+  
+  function closeForm() {
+      console.log("Close form")
+    document.getElementById("labForm").style.display = "none";
+  }
